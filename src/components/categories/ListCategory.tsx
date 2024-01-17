0d2abd91c9ebaf336e96a3e55f525c32f7a939dc
Mochammad Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2024-01-17 18:50:03 +0700

    pagination

--- src/components/categories/ListCategory.tsx
+++ src/components/categories/ListCategory.tsx
@@ -25,6 +25,17 @@ const ListCategory = () => {
 
   const filteredIngredients = filterIngredients();
 
+  const itemsPerPage = 20;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentIngredients = filteredIngredients.slice(startIndex, endIndex);
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="py-8">
       <HeadSection title="List of Ingredients" />
@@ -43,11 +54,78 @@ const ListCategory = () => {
           {filteredIngredients.length === 0 ? (
             <NotFoundData />
           ) : (
-            <div className="py-5 w-full grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 md:grid-cols-3 gap-4">
-              {filteredIngredients.map((item: ICategory, i: number) => (
-                <CategoryCard key={i} onClick={() => router.push(`/ingredient/${item.strIngredient}`) } name={item.strIngredient} />
-              ))}
-            </div>
+            <>
+              <div className="py-5 w-full grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 md:grid-cols-3 gap-4">
+                {currentIngredients.map((item: ICategory, i: number) => (
+                  <CategoryCard
+                    key={i}
+                    onClick={() =>
+                      router.push(`/ingredient/${item.strIngredient}`)
+                    }
+                    name={item.strIngredient}
+                  />
+                ))}
+              </div>
+              <div className="flex justify-center mt-4">
+                {Array.from(
+                  {
+                    length: Math.ceil(
+                      filteredIngredients.length / itemsPerPage
+                    ),
+                  },
+                  (_, i) => {
+                    const isFirstPage = i === 0;
+                    const isLastPage =
+                      i ===
+                      Math.ceil(filteredIngredients.length / itemsPerPage) - 1;
+                    const isInRange =
+                      Math.abs(i + 1 - currentPage) <= 1 ||
+                      isFirstPage ||
+                      isLastPage;
+
+                    if (isInRange) {
+                      return (
+                        <button
+                          key={i + 1}
+                          onClick={() => handlePageChange(i + 1)}
+                          className={`mx-1 md:mx-2 px-3 md:px-4 py-1 md:py-2 text-sm md:text-base ${
+                            i + 1 === currentPage
+                              ? "bg-primary text-white rounded"
+                              : "bg-gray-200 text-gray-700 rounded"
+                          }`}
+                        >
+                          {i + 1}
+                        </button>
+                      );
+                    }
+
+                    if (
+                      (currentPage >= 4 && i === 1) ||
+                      (currentPage <=
+                        Math.ceil(filteredIngredients.length / itemsPerPage) -
+                          4 &&
+                        i ===
+                          Math.ceil(filteredIngredients.length / itemsPerPage) -
+                            2)
+                    ) {
+                      return (
+                        <button
+                          key={i + 1}
+                          onClick={() =>
+                            handlePageChange(currentPage + (i === 1 ? -2 : 2))
+                          }
+                          className="mx-1 md:mx-2 px-3 md:px-4 py-1 md:py-2 text-sm md:text-base bg-gray-200 text-gray-700 rounded"
+                        >
+                          ...
+                        </button>
+                      );
+                    }
+
+                    return null;
+                  }
+                )}
+              </div>
+            </>
           )}
         </>
       )}
