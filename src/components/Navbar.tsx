4449fabd0b492008eab19691052dda2179254db7
Mochammad Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2024-01-17 19:23:24 +0700

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,10 @@ const Navbar = () => {
   return (
     <nav className="bg-white shadow sticky top-0 z-10">
       <div className="flex items-center font-medium justify-between w-full container py-3">
-        <div className="z-50 md:w-auto w-full flex justify-between">
+        <div
+          className="md:w-auto w-full flex justify-between"
+          style={{ zIndex: 999999 }}
+        >
           <h1
             className="text-primary text-2xl font-medium cursor-pointer"
             onClick={() => router.push(`/`)}
@@ -21,6 +24,7 @@ const Navbar = () => {
             {open ? <IoClose /> : <IoMenu />}
           </div>
         </div>
+
         <ul className="md:flex hidden uppercase items-center gap-8 font-[Poppins]">
           <NavLinks />
         </ul>
@@ -37,7 +41,7 @@ const Navbar = () => {
         md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-16 pl-4
         duration-500 ${open ? "left-0" : "left-[-100%]"}
         `}
-        style={{ zIndex: 999999 }}
+          style={{ zIndex: 888888 }}
         >
           <NavLinks />
           <div className="py-5">
