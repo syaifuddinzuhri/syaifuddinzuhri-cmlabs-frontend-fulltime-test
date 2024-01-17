ad181acb7b0258a4dfa698bfe181fd1e5a529430
Mochammad Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2024-01-17 17:05:15 +0700

    fix providers

--- src/utils/withProviders.tsx
+++ src/utils/withProviders.tsx
@@ -1,6 +1,12 @@
-export const withProviders = (...providers: any[]) => (
-    WrappedComponent: React.FC
-  ) => (props: Record<any, any>) =>
-    providers.reduceRight((acc, Provider) => {
+import React from "react";
+
+type ProviderType = React.FC<any>; // Adjust this based on your actual provider types
+
+export const withProviders =
+  (...providers: ProviderType[]) =>
+  (WrappedComponent: React.FC<any>) =>
+  (props: React.ComponentProps<typeof WrappedComponent>) => {
+    return providers.reduceRight((acc, Provider) => {
       return <Provider>{acc}</Provider>;
     }, <WrappedComponent {...props} />);
+  };
